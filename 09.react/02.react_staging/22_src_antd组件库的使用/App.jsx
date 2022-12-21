41de6263868c05ce687320cc956f1d31f74a6bf8
redyouzi <1589029587@qq.com>
2022-12-22 00:21:33 +0800

    change dir name

---
+++ 09.react/02.react_staging/22_src_antd组件库的使用/App.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { Button, Tooltip, DatePicker, Space } from 'antd'
+import { WechatOutlined, SearchOutlined } from '@ant-design/icons'
+const { RangePicker } = DatePicker;
+
+export default class App extends Component {
+    onChange = (date, dateString) => {
+        console.log(date, dateString);
+    };
+    render() {
+        return (
+            <div>
+                App
+                <Button type="primary">Primary Button</Button>
+                <Button>Button</Button>
+                <WechatOutlined />
+                <Button type="primary" icon={<SearchOutlined />}>
+                    Search
+                </Button>
+                <Tooltip title="小提示">
+                    <Button shape="circle" icon={<SearchOutlined />} />
+                </Tooltip>
+
+                <br />
+                <DatePicker onChange={this.onChange} />
+                <Space direction="vertical" size={12}>
+                    <RangePicker showTime />
+                </Space>
+            </div>
+        )
+    }
+}
